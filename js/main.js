760d9b2ca91717b6731acc53f1b4f3111d928bc4
Laurel Younis <laurelyounisdev@gmail.com>
2022-03-19 14:18:16 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1616,7 +1616,7 @@ function solution(number){
   return sum;
 }
 
-________________________________________________________________
+_______________________________________________________________
 You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
 
 You can not use multiplier "*" operator.
