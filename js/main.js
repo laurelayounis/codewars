3c87463e0ef84d82b7d9f7c63af7950b2b0423f2
Laurel Younis <laurelyounisdev@gmail.com>
2022-04-18 20:09:20 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1266,7 +1266,7 @@ if(number % 2 === 0){
   return number * 9
 }
 }
-______________________________________________________________________________________
+_____________________________________________________________________________________
 It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
 
 Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
