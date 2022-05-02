45b2e7d16090ae375d339f400c5c8a053acf8f4d
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-02 12:36:56 -0700

    added a 8kyu codewars

--- js/main.js
+++ js/main.js
@@ -1006,8 +1006,6 @@ if bmi > 30 return "Obese"
 
 
 
-*/
-
 
 
 function bmi (weight, height){
@@ -1034,3 +1032,12 @@ function bmi(weight, height) {
    return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
   
   }
+
+  */
+
+
+  A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+
+const hero= (bullets, dragons)=> bullets >= dragons * 2
