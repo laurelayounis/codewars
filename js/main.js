8825cab9fa01610e18583478c2e0880a3fc06104
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-11 11:15:29 -0700

    added 8 kyu codewars

--- js/main.js
+++ js/main.js
@@ -1268,3 +1268,36 @@ if(number % 2 === 0){
 }
 }
 
+It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+function bonusTime(salary, bonus) {
+  if (bonus === true) {
+    salary = salary * 10;
+    return `£${salary}`;
+  } else {
+    return `£${salary}`;
+  }
+}
+
+function bonusTime(salary, bonus) {
+  return bonus ? `£${10 * salary}` : `£${salary}`;
+}
+
+
+Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+var min = function(list){
+    
+  return Math.min(...list);
+}
+
+var max = function(list){
+  
+  return Math.max(...list);
+}
+
+
