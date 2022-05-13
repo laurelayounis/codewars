4efa2f5e7905655782f586b0fbcde5a54339cfff
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-12 19:56:33 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1300,7 +1300,7 @@ var max = function(list){
 }
 
 
-
+_______________________________________________________________________________________________________
 
 Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
 
