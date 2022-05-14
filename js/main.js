e321f4640fc0b0755b9cdc5b63f43e9dbde907a3
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-13 19:56:00 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1322,7 +1322,7 @@ function feast(beast, dish) {
   return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
 }
 
-
+____________________________________________________________________________________
 Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
 
 Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
