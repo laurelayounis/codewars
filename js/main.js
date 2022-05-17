69deb9a898fb1601728048a281d643004c1fc8da
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-17 12:31:58 -0700

    added 8kyu codewars

--- js/main.js
+++ js/main.js
@@ -1522,3 +1522,23 @@ const areaOrPerimeter = function(l , w) {
     return ((l+l) + (w + w))
   }
 };
+
+
+Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array (or list in Python, Haskell or Elixir).
+
+Examples:
+
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(x * i);
+  }
+  return result;
+}
