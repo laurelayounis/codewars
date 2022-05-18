3cbae8a11dbfc6d4927147f52b4043b719364eb1
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-18 12:55:46 -0700

    added 8 kyu codewars

--- js/main.js
+++ js/main.js
@@ -1542,3 +1542,16 @@ function countBy(x, n) {
   }
   return result;
 }
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+function otherAngle(a, b) {
+  let third = 180 - (a + b)
+  return third
+}
