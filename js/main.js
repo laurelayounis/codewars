0b57bdc2b1a730f3aa0ecbfe02cdf3a02049def0
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-19 19:55:24 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1335,7 +1335,7 @@ function capitalizeWord(word) {
 
 }
 
-
+_________________________________________________________________________________________________________
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 function removeExclamationMarks(s) {
