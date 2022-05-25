a92a0afb0c4d61915c554262bbad47f142fe67be
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-24 19:51:18 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1496,7 +1496,7 @@ function getPlanetName(id){
   
   return name;
 }
-
+______________________________________________________________________________________
 Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
 
 class Kata {
