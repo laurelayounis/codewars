360dc07ef8732fb55fb4b98e33671bc1e3895347
Laurel Younis <laurelyounisdev@gmail.com>
2022-05-30 14:03:06 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1600,3 +1600,18 @@ function firstNonConsecutive(arr) {
 
 
 ____________________________________________________________________________________
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+  let sum = 0;
+  for (var i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
