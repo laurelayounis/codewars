6e286b126b958840ca23b9a5c7f7f29d4d14f52a
Laurel Younis <laurelyounisdev@gmail.com>
2022-06-02 09:55:37 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1642,4 +1642,27 @@ function hero(bullets, dragons){
   return bullets >= 2 * dragons
   }
 
+
+________________________________________________________________________
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+ return dna.replace(/T/g,'U')
+ 
   
+}
+
+****Refactored without RegExp
+function DNAtoRNA(dna) {
+  return dna.split("T").join("U");
+}
