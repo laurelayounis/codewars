b01fda608df7df5c2b5c50dccbc43a06f2746960
Laurel Younis <laurelyounisdev@gmail.com>
2022-06-03 10:51:59 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1666,3 +1666,13 @@ function DNAtoRNA(dna) {
 function DNAtoRNA(dna) {
   return dna.split("T").join("U");
 }
+
+
+________________________________________________________________________
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc,c)=> acc + c)
+  }
