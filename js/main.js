6f4f68caa1b558304eb44be7cb263b2fa6448ce1
Laurel Younis <laurelyounisdev@gmail.com>
2022-06-12 14:56:34 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1807,7 +1807,7 @@ function setAlarm(employed, vacation){
 
 }
 
-____________________________
+________________________________
 You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
 Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
