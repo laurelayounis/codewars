9e3194fbb5886742db8fbe07bc6235abe96e3f4b
Laurel Younis <laurelyounisdev@gmail.com>
2022-06-13 15:55:02 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1828,7 +1828,7 @@ function updateLight(current) {
 }
 
 
-____________________________
+________________________________
 
 Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
