6c464b7419bf1671a1a7b4a13b3e12601d9378c1
Laurel Younis <laurelyounisdev@gmail.com>
2022-06-25 09:00:09 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1884,5 +1884,5 @@ function getGrade (s1, s2, s3) {
   return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
 }
 
-
+asdfsdfasdfsafas
 lllll aasdfsddfs safasdfadsfsdfsadfsfa  asdfasdfa sad;fjssfsdafassadkfl asd;lfs;afdjasadfasdf
