f1224a62d2966a4703d99e26d067919cc1407b80
Laurel Younis <laurelyounisdev@gmail.com>
2022-07-28 15:52:17 -0700

    06/13/2022
    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1826,3 +1826,21 @@ function updateLight(current) {
   }
 
 }
+
+
+____________________________
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+  //Code here
+  str= str.split(' ')
+  .map(e => e === '?' || e === '!' ? e: e= e.slice(1) + e.slice(0,1) + 'ay')
+  .join(' ')
+  
+  return str
+}
