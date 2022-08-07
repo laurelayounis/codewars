dca113e0e24b6340d21a901acc326b98485f0790
Laurel Younis <laurelyounisdev@gmail.com>
2022-08-07 16:06:31 -0700

    daily coding challenge

--- js/main.js
+++ js/main.js
@@ -1907,7 +1907,7 @@ function shortcut (string) {
 dfsdfasfgsdag
 sdgsdgasadas
 sfasfsafsagassdfasdfdg
-sdgsdggasdfsdafasasdfsdfdfasdfsf
+sdgsdggasdfsdafasasdfsdfdfasdfssafsadff
 adfasgsagsgsgssffsfasfasgasdgadgsadga
 asfasgasgsgasdgasagasdgsgsdgsdfasdsafdsdaf
 asdfsfsasafsdafsfsafsdfagsadgadgasgasgasdgasasfdsafdg
